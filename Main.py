0d06bd5b4fd8e2bfdc71858dbb4ecffdc9c4e78a
Darren Kai Mew <darrenk.mew@gmail.com>
2021-09-03 10:35:41 +0200

    Create Main.py
    All Game logic added. 
    3 Questions

---
+++ Main.py
@@ -0,0 +1,76 @@
+def main():
+    print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+    print("Welcome to Treasure Island.")
+    print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+ans1=input("You Are Looking for a treasure and every journey stars somewhere so 'left' or 'right' : \n")
+
+def Dead():
+    print(''' **********************************YOU ARE VERY DEAD*************************
+    
+                            ,--.
+                           {    }
+                           K,   }
+                          /  ~Y`
+                     ,   /   /
+                    {_'-K.__/
+                      `/-.__L._
+                      /  ' /`\_}
+                     /  ' /
+             ____   /  ' /
+      ,-'~~~~    ~~/  ' /_
+    ,'             ``~~~  ',
+   (                        Y
+  {                         I
+ {      -                    `,
+ |       ',                   )
+ |        |   ,..__      __. Y
+ |    .,_./  Y ' / ^Y   J   )|
+ \           |' /   |   |   ||
+  \          L_/    . _ (_,.'(
+   \,   ,      ^^""' / |      )
+     \_  \          /,L]     /
+       '-_~-,       ` `   ./`
+          `'{_            )
+              ^^\..___,.--`
+    ''')
+
+main()
+if(ans1.lower()=='right'):
+    Dead()
+else: 
+    ans2=input("You find a map to the treasure. Buy or Leave : \n")
+    if(ans2.lower()=="leave"): 
+        print("Look at this guy trying to find treasure with no map the people who hired you")
+        Dead()
+    else: 
+        print("How did you buy a map with no money ? Ph yes your a pirate you dont buy you plunder ")
+        ans3=input("You Are Looking for a treasure and your have a map, There is a big pond what do you do : \n")
+        if(ans3.count('s')>1): 
+            print("You got the treasure")
+        else:
+            print("You jump into lava really ")
+            Dead()
